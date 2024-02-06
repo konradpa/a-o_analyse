54b378c8d0f4996da4bcdc633e3aba1aa5ed12bf
konradpa <konrad@pagenstedt.de>
2024-02-06 12:21:21 +0100

    update function

--- Analyse.R
+++ Analyse.R
@@ -5,14 +5,31 @@ source("functions.R")
 
 install.packages("readxl")
 library("readxl")
-excel_file_path <- "Daten/Fragebogendaten_Mittelwerte_Seminar_B_Team_1.xlsx"
 
-vr_questionaire_group1 <- read_excel(excel_file_path, sheet = "VR_Meeting")
+vr_group1 <- read_excel("Daten/Fragebogendaten_Mittelwerte_Seminar_B_Team_1.xlsx", sheet = "VR_Meeting")
 
-zoom_questionaire_group1 <- read_excel(excel_file_path, sheet = "Zoom-Meeting")
+zoom_group1 <- read_excel("Daten/Fragebogendaten_Mittelwerte_Seminar_B_Team_1.xlsx", sheet = "Zoom-Meeting")
+
+vr_external <- read_excel("Daten/Entitativity _VR_Team 1.xlsx", sheet = "VR_Meeting_Gesamt")
+
+zoom_external <- read_excel("Daten/Entitativity _Zoom_Team 1.xlsx", sheet = "Zoom_Meeting_Gesamt")
 
 # Cleaning Data up
 
-vr_questionaire_group1<-clean_data(vr_data)
+clean_vr_group1<-clean_data_group1(vr_group1)
 
-zoom_questionaire_group1<-clean_data(zoom_data)
+clean_zoom_group1<-clean_data_group1(zoom_group1)
+
+clean_vr_external <- clean_data_external(vr_external)
+
+clean_zoom_external <- clean_data_external(zoom_external)
+
+# Ratings: External: Zoom vs. VR
+
+plot_mean_gesamt(clean_zoom_external, clean_vr_external, "Zoom external", "VR external ratings")
+
+plot_mean_gesamt(clean_zoom_group1, clean_vr_group1)
+
+
+
+
