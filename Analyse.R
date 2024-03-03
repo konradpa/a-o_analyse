acd3975a14f91da28e8e349bef5b802e00052319
konradpa <135045175+konradpa@users.noreply.github.com>
2024-03-03 21:46:43 +0100

    add cohens D analysis for self vs external

--- Analyse.R
+++ Analyse.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 # Getting data
 source("functions.R")
 
@@ -24,13 +26,22 @@ zoom_interact <- clean_data_interact("Daten/Ausgewertet_Sem B_Team 6_Zoom.xls")
 vr_interact<- clean_data_interact("Daten/Ausgewertet_Sem B_Team 6_VR.xls")
 
 # Calculating ES for phase one vs phase two
-# Calculate Cohen's d for clean_vr_group1
+## Calculate Cohen's d for clean_vr_group1
 cohen_d_vr <- calculate_cohens_d(clean_vr_self, 9, 10)
 cat("Cohen's d for VR data:", cohen_d_vr, "\n")
 
-# Calculate Cohen's d for clean_zoom_group1
+## Calculate Cohen's d for clean_zoom_group1
 cohen_d_zoom <- calculate_cohens_d(clean_zoom_self, 9, 10)
-cat("Cohen's d for Zoom data:", cohen_d_zoom, "\n")
+cat("Cohen's d for Zoom data:", cohen_d_zoom, "\n"
+      )
+
+## Calculate Cohen's d for self vs external: VR
+relevant_vars <- c("Entitativität", "Ähnlichkeit", "Interaktivität", "Geteilte Ziele")
+
+calculate_cohens_d_multiple(clean_vr_external, clean_vr_self,relevant_vars)
+
+## Zoom
+calculate_cohens_d_multiple(clean_zoom_external, clean_zoom_self,relevant_vars)
 
 # Testing the external Ratings for normal distribution
 ## Apply the function to clean_zoom_external and clean_vr_external
@@ -45,20 +56,17 @@ print(shapiro_test_vr)
 
 
 # T-Sum Test: 
-rows_self <- 5:8  # Rows containing self-ratings in clean_..._group1
-rows_external <- 1:4  # Rows containing external ratings in clean_..._external
 # Ratings: Self vs. External: Zoom
-results_zoom <- perform_tsum_test(clean_zoom_external, clean_zoom_self, rows_external, rows_self)
+results_zoom <- perform_tsum_test(clean_zoom_external, clean_zoom_self, indices_external, indices_self)
 
 ## Ratings: Self vs. External: VR
-results_vr <-   perform_tsum_test(clean_vr_external, clean_vr_self, rows_external, rows_self)
+results_vr <-   perform_tsum_test(clean_vr_external, clean_vr_self, indices_external, indices_self)
 
-print(results_zoom)
-print(results_vr)
+
 
 # Alternative way
-## perform_paired_t_tests(clean_vr_self, clean_vr_external, rows_self, rows_external)
-## perform_paired_t_tests(clean_zoom_self, clean_zoom_external, rows_self, rows_external)
+perform_paired_t_tests(clean_vr_self, clean_vr_external, rows_self, rows_external)
+perform_paired_t_tests(clean_zoom_self, clean_zoom_external, rows_self, rows_external)
 
 
 
